9d31a8e09067593530d6505a539e32c89c71e130
NehuenCuenca <nehuen.cuenca@hotmail.com>
2023-06-23 00:22:03 -0300

    try 1, fix toggle navBar and buttons on hamburguer menu

--- main.js
+++ main.js
@@ -5,23 +5,22 @@ const closeHamburguerMenuBtn = header.querySelector('#close-hamburguer-menu');
 const navBar = header.querySelector('nav#navbar');
 
 const unloadHamburguerMenu = () => {
-    navBar.classList.remove('mobile', 'hidden')
-    hamburguerMenuBtn.classList.add('hidden')
-    closeHamburguerMenuBtn.classList.add('hidden')
+    navBar.classList.remove('mobile', 'hidden') // Display navbar for desktop
+    hamburguerMenuBtn.classList.add('hidden') // Hide hamburguerMenuBtn button 
+    closeHamburguerMenuBtn.classList.add('hidden') // Hide closeHamburguerMenuBtn utton  
 } 
 
 const loadHamburguerMenu = () => { 
-    navBar.classList.toggle('hidden') // Hide the nav links
-    hamburguerMenuBtn.classList.toggle('hidden') // Display hamburguer button
-    closeHamburguerMenuBtn.classList.toggle('hidden') // Display '✖' hamburguer button (close button)
-    // debugger
+    navBar.classList.add('hidden') // Hide the nav links
+    hamburguerMenuBtn.classList.remove('hidden') // Display hamburguer button
+    closeHamburguerMenuBtn.classList.remove('hidden') // Display '✖' hamburguer button (close button)
     
     // WHEN OPEN THE MENU...
     hamburguerMenuBtn.addEventListener('click', () => { navBar.classList.add('mobile') })
 
     // WHEN CLOSE THE MENU...
     closeHamburguerMenuBtn.addEventListener('click', () => { navBar.classList.remove('mobile') })
- }
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
